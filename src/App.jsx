5fcc584bdbc708d83210a9cbd3669d01326fb388
Arslan <66740974+Arslan-Soomro@users.noreply.github.com>
2021-09-26 10:56:26 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">Welcome To Vite + React + Tailwind JIT Project Template</div>
+  )
+}
+
+export default App;
